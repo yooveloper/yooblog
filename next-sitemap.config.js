1a0ad5a6f19e2ebc4d525f072b82d96fc48246f3
yooveloper <yooveloper@gmail.com>
2022-11-22 13:57:19 +0900

    🔧 add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  siteUrl: 'https://yooveloper.dev',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
