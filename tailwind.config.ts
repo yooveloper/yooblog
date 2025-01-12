cad17ad70a5a8d95033f7491624e594efa542775
yooveloper <yooveloper@gmail.com>
2025-01-12 21:09:11 +0900

    feat: 폰트 설정 추가(pretendard, jetbrainsMono

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,16 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
+      fontFamily: {
+        pretendard: ['var(--font-pretendard'],
+        jetbrainsMono: ['var(--font-jetbrainsMono)'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
